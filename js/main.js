b6418cbf7f1eae164677a129af4ac7fd9b41ea95
jesusurbina070 <jr.diurbina@gmial.com>
2020-04-09 00:23:32 -0430

    colocamos animaciones con js y keyframes en la seccion founders

--- js/main.js
+++ js/main.js
@@ -33,3 +33,36 @@ const ipad = window.matchMedia('screen and (max-width: 480px)')
 			}
 			
 			}
+
+window.addEventListener('scroll', function(){
+
+    let img1 = document.getElementById('img1')
+    let img2 = document.getElementById('img2')
+    let img3 = document.getElementById('img3')
+	let person1 = document.getElementById('person1')
+	let person2 = document.getElementById('person2')
+	let person3 = document.getElementById('person3')
+	let posicionObj1 = person1.getBoundingClientRect().top
+	let posicionObj2 = person2.getBoundingClientRect().top
+	let posicionObj3 = person3.getBoundingClientRect().top
+	let tamañoPantalla = window.innerHeight/2.5
+	console.log(posicionObj1)
+
+	if( posicionObj1 < tamañoPantalla){
+		img1.style.animation = 'topBottom 1s forwards ease'
+	}
+	if( posicionObj2 < tamañoPantalla){
+		img2.style.animation = 'topBottom 1s forwards ease'
+	}
+
+	if( posicionObj3 < tamañoPantalla){
+		img3.style.animation = 'topBottom 1s forwards ease'
+	}
+})
+
+
+
+
+
+
+
